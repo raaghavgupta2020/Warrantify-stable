a9fee2d70f6a20b7b389829f0ae0d7423eea6774
raaghavgupta2020 <raaghavgupta2020@gmail.com>
2022-07-28 23:53:46 +0530

    smallchanges

--- frontend/src/components/creatorprofile/pages/Blog/BlogItem.jsx
+++ frontend/src/components/creatorprofile/pages/Blog/BlogItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import '../../../nft_card.css'
 
 const BlogItem = (props) => {
 	const { image, title, description, type, price, expiry } = props.blog;
 	return (
+		
 		<div className="w-full lg:w-1/2">
+			
 			<div className="my-4 md:mx-4 shadow p-6 rounded-md bg-white group hover:shadow-md">
+			{/* <div class="nft"> */}
 				<div className="relative mb-6 w-full h-56 bg-purple-200 rounded-md overflow-hidden">
 					{type ? (
 						<video
@@ -16,26 +20,35 @@ const BlogItem = (props) => {
 						<img
 							src={image}
 							alt="blogImage"
-							className="w-full h-full object-cover object-center transform group-hover:scale-125 group-hover:rotate-6 transition duration-200"
+							class='tokenImage h-full'
+							// className="w-full h-full object-cover object-center transform group-hover:scale-125 group-hover:rotate-6 transition duration-200"
 						/>
 					)}
 				</div>
 				<h3>
 					<a
 						href="#0"
-						className="block text-lg font-medium text-gray-800 hover:text-purple-600 mb-2"
+						className="block text-lg font-medium text-black hover:text-purple-600 mb-2"
 					>
 						{title}
+						
 					</a>
 				</h3>
-        <p className="text-gray-400">{description}</p>
+				
+        <p className="text-black" >{description}</p>
+		<br/>
         <div className="flex justify-between" >
-          <p>Amount {price} ETH</p>
-		  <p>Warranty Period : {expiry} seconds</p>
-          <button onClick={() => props.buyNft(props.blog)}>Buy Product</button>
+			<div class="price">
+          <p className="text-black">Amount {price} ETH</p>
+		  </div>
+		  <p className="text-black">Warranty Period : {expiry} seconds</p>
+          <button className="text-black border-2 rounded-lg pl-2 pr-2 " onClick={() => props.buyNft(props.blog)}>Buy Product</button>
         </div>
+		{/* </div> */}
 			</div>
 		</div>
+
+		
 	);
 };
 
